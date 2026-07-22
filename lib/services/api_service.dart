e79d1fa2979d203983e1e8c267a3a19010d1ae4f
SulemanAI <muhammadsuleman0759@gmail.com>
2026-07-22 05:18:41 +0500

    Update mobile app API base URL to live Render backend

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -19,20 +19,7 @@ class ApiService {
       return _configuredBaseUrl;
     }
 
-    if (kIsWeb) {
-      return 'http://localhost:8000';
-    }
-
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return 'http://10.0.2.2:8000';
-      case TargetPlatform.iOS:
-      case TargetPlatform.linux:
-      case TargetPlatform.macOS:
-      case TargetPlatform.windows:
-      case TargetPlatform.fuchsia:
-        return 'http://localhost:8000';
-    }
+    return 'https://disaster-sense-1.onrender.com';
   }
 
   Uri _uri(String path) => Uri.parse('$baseUrl$path');
